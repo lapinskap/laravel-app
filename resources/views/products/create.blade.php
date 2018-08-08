6e9822294e2834c7edcfff8cc0156928d3f00edf
Paulina <cherry3475@gmail.com>
2018-08-08 09:25:27 +0200

    add product view

---
+++ resources/views/products/create.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Laravel 5.5 CRUD Tutorial With Example From Scratch </title>
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+  </head>
+  <body>
+    <div class="container">
+      <h2>Create A Product</h2><br  />
+      <form method="post">
+        <div class="row">
+          <div class="col-md-4"></div>
+          <div class="form-group col-md-4">
+            <label for="name">Name:</label>
+            <input type="text" class="form-control" name="name">
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-4"></div>
+            <div class="form-group col-md-4">
+              <label for="price">Price:</label>
+              <input type="text" class="form-control" name="price">
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-4"></div>
+          <div class="form-group col-md-4">
+            <button type="submit" class="btn btn-success" style="margin-left:38px">Add Product</button>
+          </div>
+        </div>
+      </form>
+    </div>
+  </body>
+</html>
